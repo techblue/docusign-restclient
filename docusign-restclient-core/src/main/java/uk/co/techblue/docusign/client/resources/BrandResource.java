6a98953a44f6a0850de7c2902f8ee9bcb9ee1ed3
Dishant Anand <dishant.anand@techblue.co.uk>
2014-10-26 12:20:42 +0000

    Added resource service for updating the branding information

--- docusign-restclient-core/src/main/java/uk/co/techblue/docusign/client/resources/BrandResource.java
+++ docusign-restclient-core/src/main/java/uk/co/techblue/docusign/client/resources/BrandResource.java
@@ -3,6 +3,7 @@ package uk.co.techblue.docusign.client.resources;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -65,6 +66,20 @@ public interface BrandResource extends Resource {
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public ClientResponse<EsignBranding> deleteEsignBrandsByGroupId(@PathParam("groupid") final String groupId,
+            final EsignBranding esignBranding);
+
+    /**
+     * Update esign brands by group id.
+     * 
+     * @param groupId the group id
+     * @param esignBranding the esign branding
+     * @return the client response
+     */
+    @PUT
+    @Path("/groups/{groupid}/brands")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public ClientResponse<EsignBranding> updateEsignBrandsByGroupId(@PathParam("groupid") final String groupId,
             final EsignBranding esignBranding);
 
 }
