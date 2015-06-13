79e063b9bd998376fee5fb3d7503decbe00bc236
Marco Altieri <marco.altieri@gmail.com>
2015-06-13 17:26:37 +0100

    Removed unnecessary imports. Fix for UTC date parsing
    The class AuditEvent uses SimpleDateFormat to parse dates returned by
    DocuSign. This class does not correctly handle dates. It is necessary to
    modify the date before parsing it.
    It could be better to use Joda instead.

--- docusign-restclient-dto/src/main/java/uk/co/techblue/docusign/client/dto/tabs/AbstractRecipientTab.java
+++ docusign-restclient-dto/src/main/java/uk/co/techblue/docusign/client/dto/tabs/AbstractRecipientTab.java
@@ -1,6 +1,7 @@
 package uk.co.techblue.docusign.client.dto.tabs;
 
 public abstract class AbstractRecipientTab extends Tab {
+	private static final long serialVersionUID = -526444511551408649L;
 	public enum TabTypes {ApproveTab, CheckboxTab, CompanyTab, 
 		DateSignedTab, DateTab, DeclineTab, EmailTab, FullNameTab,
 		InitialHereTab, NoteTab, NumberTab, SignHereTab, SSNTab, 
